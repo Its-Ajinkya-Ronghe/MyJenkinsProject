27d0e9cd4da1370970f16d4a4a9bf866e26cbf2a
Ajinkya Ronghe <rongheajinkya72@gmail.com>
2026-04-07 14:42:46 +0530

    final

--- Excel_Sheet_Parallel_Testing/src/test/java/stepdefinitions/LoginSteps.java
+++ Excel_Sheet_Parallel_Testing/src/test/java/stepdefinitions/LoginSteps.java
@@ -3,6 +3,7 @@ package stepdefinitions;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.remote.*;
+<<<<<<< HEAD
 import utils.ExcelReader;
 
 import java.net.URL;
@@ -15,6 +16,21 @@ public class LoginSteps {
 
     RemoteWebDriver driver;
     static List<String[]> loginData = ExcelReader.getLoginData("login.xls");
+=======
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.ExcelReader;
+import org.junit.Assert;
+
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+
+public class LoginSteps {
+
+    WebDriver driver; // Changed to WebDriver interface for flexibility
+    static List<String[]> loginData = ExcelReader.getLoginData("login.xlsx");
+>>>>>>> cb7c773 (initial commit)
     static int index = 0;
 
     String currentUsername;
@@ -25,6 +41,7 @@ public class LoginSteps {
     public void openBrowser() throws Exception {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setBrowserName("chrome");
+<<<<<<< HEAD
         driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
         driver.manage().window().maximize();
 
@@ -34,6 +51,21 @@ public class LoginSteps {
             currentUsername = row[0];
             currentPassword = row[1];
             expectedStatus = row[2];
+=======
+
+        // Ensure your Selenium Grid is running at this URL!
+        driver = new RemoteWebDriver(new URL("http://172.21.0.197:4444/wd/hub"), caps);
+        driver.manage().window().maximize();
+
+        // Thread-safe check for index
+        synchronized (LoginSteps.class) {
+            if (index < loginData.size()) {
+                String[] row = loginData.get(index++);
+                currentUsername = row[0];
+                currentPassword = row[1];
+                expectedStatus = row[2];
+            }
+>>>>>>> cb7c773 (initial commit)
         }
     }
 
@@ -44,6 +76,7 @@ public class LoginSteps {
 
     @When("user enters username {string} and password {string}")
     public void enterCredentials(String u, String p) {
+<<<<<<< HEAD
         driver.findElement(By.id("userName")).sendKeys(currentUsername);
         driver.findElement(By.id("password")).sendKeys(currentPassword);
         driver.findElement(By.id("login")).click();
@@ -65,5 +98,43 @@ public class LoginSteps {
         }
 
         driver.quit();
+=======
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Wait for elements to be visible before typing
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("userName"))).sendKeys(currentUsername);
+        driver.findElement(By.id("password")).sendKeys(currentPassword);
+
+        // Use JavascriptExecutor if the login button is hidden by ads on DemoQA
+        WebElement loginBtn = driver.findElement(By.id("login"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", loginBtn);
+    }
+
+    @Then("login should be {string}")
+    public void verifyLogin(String status) {
+        boolean loginSuccess;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        try {
+            // On DemoQA, the "Log out" button (id=submit) appears only after successful login
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("submit")));
+            loginSuccess = true;
+        } catch (TimeoutException e) {
+            loginSuccess = false;
+        }
+
+        try {
+            if (expectedStatus.equalsIgnoreCase("pass")) {
+                Assert.assertTrue("Login expected to PASS but it FAILED", loginSuccess);
+            } else {
+                Assert.assertFalse("Login expected to FAIL but it PASSED", loginSuccess);
+            }
+        } finally {
+            // Ensure the browser closes even if the assertion fails
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+>>>>>>> cb7c773 (initial commit)
     }
 }
--- Excel_Sheet_Parallel_Testing/src/test/resources/features/Login.feature
+++ Excel_Sheet_Parallel_Testing/src/test/resources/features/Login.feature
@@ -7,5 +7,19 @@ Feature: DemoQA Login Test with Excel
     Then login should be "<status>"
 
     Examples:
+<<<<<<< HEAD
       | username     | password    | status  |
       | excel        | excel       | excel   |
+=======
+      | username     | password    | status |
+      | validUser1@  | validPass1@ | pass   |
+      | validUser2@  | validPass2@ | fail   |
+      | validUser3   | validPass3  | fail   |
+      | validUser4   | validPass4  | fail   |
+      | validUser5   | validPass5  | fail   |
+      | validUser6   | validPass6  | fail   |
+      | validUser7   | validPass7  | fail   |
+      | validUser8   | validPass8  | fail   |
+      | invalidUser1 | wrongPass1  | fail   |
+      | invalidUser2 | wrongPass2  | fail   |
+>>>>>>> cb7c773 (initial commit)
